b928e512a17b728b2ecf6aca2b597ac73ba7d296
Developer <Developer@LAPTOP-C700UU2G>
2023-06-29 22:13:17 -0300

    Add project files.

---
+++ Blog.Domain/Models/News.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Domain.Models;
+
+public class News
+{
+	public int Id { get; set; }
+	public string Title { get; set; }
+	public string Description { get; set; }
+	public string Content { get; set; }
+	public DateTime PublicationDate { get; set; }
+	public IList<Tag> Tags { get; set; }
+	public Author Author { get; set; }
+}
---
+++ Blog.Infra/Contexts/BlogContext.cs
@@ -0,0 +1,24 @@
+﻿using Blog.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Infra.Contexts
+{
+	public class BlogContext : DbContext
+	{
+		public BlogContext(DbContextOptions options) : base(options) { }
+
+		DbSet<Tag> Tags { get; set; }
+		DbSet<Author> Authors { get; set; }
+		DbSet<News> Tidings { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfigurationsFromAssembly(typeof(BlogContext).Assembly);
+		}
+	}
+}
